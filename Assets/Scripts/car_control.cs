ee4cc359dc924cbdeb844014a14b5b7e3d1878e0
Shaurya Pant <shauryapant@gmail.com>
2019-02-07 16:50:31 +0530

    Add files

---
+++ Assets/Scripts/car_control.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car_control : MonoBehaviour
+{
+    public float CarSpeed;
+    public float maxpos;
+    public float minpos;
+    Vector3 Position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Position = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Position.x += CarSpeed * Time.deltaTime;
+            Position.x = Mathf.Clamp(Position.x, minpos, maxpos);
+            transform.position = Position;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Position.x -= CarSpeed * Time.deltaTime;
+            Position.x = Mathf.Clamp(Position.x, minpos, maxpos);
+            transform.position = Position;
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "enemy")
+        {
+            Destroy(gameObject);
+
+        }
+    }
+}
